4fb3ac85ddd8b2b3f075d0538b19df4f496b0f97
tusharkhavale <tushar2717@gmail.com>
2017-03-09 19:54:43 +0530

    Added popup screen on game start

--- Assets/UnityPitchControl/_Pitch/InputManager.cs
+++ Assets/UnityPitchControl/_Pitch/InputManager.cs
@@ -12,14 +12,19 @@ namespace UnityPitchControl.Input {
 		public float[] _samples;
 		public PitchTracker _pitchTracker;
 		public List<int> _detectedPitches;
+		public float spectralPitch;
+		public Text txtFrequency;
 		public Text txtPitch;
 		AudioSource audioPlayer;
 		int sampleRate = 44000;      // Not sure if 44000 works on device so usiing AudioSettings.outputSampleRate on line 27
 		int binSize = 1024;
 		float[] harmonics;
+		bool isPlaying;
+
+
 
 		private static InputManager _instance;
-		void Start() 
+		public void StartRecording() 
 		{
 			_instance = UnityEngine.Object.FindObjectOfType(typeof(InputManager)) as InputManager;
 			if (_instance == null) 
@@ -39,12 +44,13 @@ namespace UnityPitchControl.Input {
 				}
 				_instance = UnityEngine.Object.FindObjectOfType(typeof(InputManager)) as InputManager;
 			}
+				// Start recording
 
-				// start recording
-				int minFreq, maxFreq;
-				Microphone.GetDeviceCaps(_audioDevice, out minFreq, out maxFreq);
-				if (minFreq > 0) _micInput = Microphone.Start(_audioDevice, true, 1, minFreq);
-				else _micInput = Microphone.Start(_audioDevice, true, 1, sampleRate);
+				// Not dependable GetDeviceCaps
+//				int minFreq, maxFreq;
+//				Microphone.GetDeviceCaps(_audioDevice, out minFreq, out maxFreq);
+//				if (minFreq > 0) _micInput = Microphone.Start(_audioDevice, true, 1, minFreq);
+				_micInput = Microphone.Start(_audioDevice, true, 1, sampleRate);
 				audioPlayer = GetComponent<AudioSource> ();
 				audioPlayer.clip = _micInput;
 				audioPlayer.Play ();
@@ -55,19 +61,65 @@ namespace UnityPitchControl.Input {
 				_pitchTracker.SampleRate = _micInput.samples;
 				_pitchTracker.PitchDetected += new PitchTracker.PitchDetectedHandler(PitchDetectedListener);
 				spectrumData = new float[binSize];
+				isPlaying = true;
 		}
 
 		float[] spectrumData;
 		public void Update() 
 		{
+			// Game not startted or paused
+			if (!isPlaying)
+				return;
+
 			_detectedPitches.Clear(); // clear pitches from last update
 			_micInput.GetData(_samples, 0);
 			audioPlayer.GetSpectrumData (spectrumData, 0, FFTWindow.BlackmanHarris);
-//			AudioVisualizer.GetInstance ().UpdateVisualizer (spectrumData);
+			FindPeakHarmonic ();
+			AudioVisualizer.GetInstance ().UpdateVisualizer (spectrumData);
 			_pitchTracker.ProcessBuffer(_samples);
 				
 		}
 
+		void FindPeakHarmonic()
+		{
+			float bin = 0;
+			int index = 0;
+			for (int i = 0; i < spectrumData.Length; i++) 
+			{
+				if (spectrumData [i] > bin) 
+				{
+					bin = spectrumData [i];
+					index = i;
+				}
+			}
+			float maxV = spectrumData[index];
+			int maxN = index;
+			float freqN = maxN; // pass the index to a float variable
+			if (maxN > 0 && maxN < binSize - 1)
+			{ // interpolate index using neighbours
+				var dL = spectrumData[maxN - 1] / spectrumData[maxN];
+				var dR = spectrumData[maxN + 1] / spectrumData[maxN];
+				freqN += 0.5f * (dR * dR - dL * dL);
+			}
+			spectralPitch = freqN * (sampleRate / 2f) / binSize;
+		}
+
+		// Stop recording
+		public void StopRecording()
+		{
+			isPlaying = false;
+			Microphone.End(_audioDevice);
+		}
+
+		// End game and go back to Game Selection Screen
+		public void EndGame()
+		{
+			StopRecording ();
+			_pitchTracker = null;
+			GameController.gameController.uiManager.UIScreenTransition (EScreen.SelectGame);
+		}
+
+		// Pitch detected callback
 		private void PitchDetectedListener(PitchTracker sender, PitchTracker.PitchRecord pitchRecord) 
 		{
 			int pitch = (int)Math.Round(pitchRecord.Pitch);
@@ -79,8 +131,15 @@ namespace UnityPitchControl.Input {
 				if (p > 0 && p < lowestPitch)
 					lowestPitch = p;
 			}
-			txtPitch.text = lowestPitch +" Hz";
 
+			// Check against spectral pitch
+			lowestPitch = spectralPitch > 1500 ? (int)spectralPitch : lowestPitch; 
+
+			// Render pitch and Frequency on screen
+			txtFrequency.text = lowestPitch +" Hz";
+			txtPitch.text = FrequencyMapping.GetInstance().GetNote(lowestPitch);
+				
+			// calculate fundamental frequency bin
 			float freqN = lowestPitch * binSize*2f/sampleRate;
 			int index = (int)freqN;     // Not using Matf.RoundToInt because lower int value required and not nearest
 			if (pitch != 0)
@@ -89,6 +148,8 @@ namespace UnityPitchControl.Input {
 				ChakraLongTone.GetInstance ().NormalizeChakras ();		
 		}
 
+
+		// Calculate amplitude of harmoics
 		float[] GetHarmoicsAmplitude(float[] spectrum,int peakBin, int windowHalfLen, int harmonic)
 		{
 			float[] harmonicsData = new float[harmonic];
